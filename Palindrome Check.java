289aee9104d483f23b4195a7ea2a9c8ebbe0cc0a
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-02-04 20:02:22 +0530

    Update

---
+++ Palindrome Check.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int original = num, reversed = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+
+        if (original == reversed) {
+            System.out.println(original + " is a Palindrome.");
+        } else {
+            System.out.println(original + " is not a Palindrome.");
+        }
+        scanner.close();
+    }
+}
