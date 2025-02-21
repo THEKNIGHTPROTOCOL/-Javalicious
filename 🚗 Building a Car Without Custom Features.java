e529f3b706e2cb7978905969e0bef9ac0803b71b
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-02-21 21:02:44 +0530

    Update

---
+++ 🚗 Building a Car Without Custom Features.java
@@ -0,0 +1,9 @@
+class Car {
+    Car() {  // Constructor
+        System.out.println("A new car is created!");
+    }
+
+    public static void main(String[] args) {
+        Car myCar = new Car(); // The constructor runs automatically
+    }
+}
