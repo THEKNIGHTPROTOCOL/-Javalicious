5bc2e646bbc4a567c07ce115ecab0c259081ad04
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-08 21:52:49 +0530

    Create interface Printable.java

---
+++ interface Printable.java
@@ -0,0 +1,25 @@
+interface Printable {
+    void print();
+}
+
+interface Showable {
+    void show();
+}
+
+class Document implements Printable, Showable {
+    public void print() {
+        System.out.println("Printing Document");
+    }
+
+    public void show() {
+        System.out.println("Showing Document");
+    }
+}
+
+public class TestMultipleInheritance {
+    public static void main(String[] args) {
+        Document doc = new Document();
+        doc.print();
+        doc.show();
+    }
+}
