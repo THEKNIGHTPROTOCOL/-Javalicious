d7239b5024f2648602c1bf4bab68191a17e93225
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-08 16:57:08 +0530

    Create FOR and WHILE Example.java

---
+++ FOR and WHILE Example.java
@@ -0,0 +1,17 @@
+public class LoopDemo {
+    public static void main(String[] args) {
+        // For loop
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // While loop
+        System.out.println("While Loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
