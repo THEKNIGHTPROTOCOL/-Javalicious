c070011b55ddb856a0c805354e04df00f7de4922
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-08 19:49:42 +0530

    Create AnonymousExample.java

---
+++ AnonymousExample.java
@@ -0,0 +1,12 @@
+public class AnonymousExample {
+    public static void main(String[] args) {
+        Runnable obj = new Runnable() {
+            public void run() {
+                System.out.println("Anonymous inner class implementing Runnable");
+            }
+        };
+
+        Thread t = new Thread(obj);
+        t.start();
+    }
+}
