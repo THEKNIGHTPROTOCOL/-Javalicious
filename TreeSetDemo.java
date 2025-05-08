eb25b3047a2d4e1b3435b017c7e4e293e7b0a35c
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-08 14:47:19 +0530

    Create TreeSetDemo.java

---
+++ TreeSetDemo.java
@@ -0,0 +1,9 @@
+import java.util.TreeSet;
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet<String> names = new TreeSet<>();
+        names.add("Zebra");
+        names.add("Apple");
+        System.out.println(names); // Output: [Apple, Zebra]
+    }
+}
