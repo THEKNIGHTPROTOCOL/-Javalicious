e2bd8a5c88ede511cb71811713165e45d7639a5f
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-09 14:06:27 +0530

    Create mypackageMyClass.java

---
+++ mypackageMyClass.java
@@ -0,0 +1,8 @@
+import mypackage.MyClass;  // Import the class
+
+public class Test {
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+}
