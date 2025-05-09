b54b54ce310a07c9208c40817a5931ff9c2db893
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-09 15:30:19 +0530

    Create AgeException.java

---
+++ AgeException.java
@@ -0,0 +1,19 @@
+class AgeException extends Exception {
+    AgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        try {
+            int age = 15;
+            if (age < 18)
+                throw new AgeException("Not eligible to vote");
+            else
+                System.out.println("Eligible");
+        } catch (AgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
