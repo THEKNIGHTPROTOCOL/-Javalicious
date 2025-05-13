a5a8632365b64e44d54ff3d111a6fffcd4815941
Sankalp <160457369+GUARDIANSOFCODE@users.noreply.github.com>
2025-05-13 14:18:46 +0530

    Create Syntax for exception.java

---
+++ Syntax for exception.java
@@ -0,0 +1,9 @@
+try {
+    // Code that may throw exception
+} catch (ExceptionType1 e1) {
+    // Handle exception
+} catch (ExceptionType2 e2) {
+    // Handle exception
+} finally {
+    // Code that will always execute
+}
