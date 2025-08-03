77eb20bf8947745980eedb20e72d9983a4c6f24a
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-03 11:22:58 +0530

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,7 +1,7 @@
 class Animal {  
     void sound() { System.out.println("Animal makes a sound"); }  
 }  
-      
+           
 class Dog extends Animal {  
     void sound() { System.out.println("Dog barks"); }  
 }
