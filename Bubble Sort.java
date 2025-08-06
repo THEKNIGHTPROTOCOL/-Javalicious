81969f4fbfa140775206c91a7107dd1b935825ff
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-06 08:52:38 +0530

    Update Bubble Sort.java

--- Bubble Sort.java
+++ Bubble Sort.java
@@ -6,7 +6,7 @@ class BubbleSort {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                     arr[j + 1] = temp;
                 }
     }
 
