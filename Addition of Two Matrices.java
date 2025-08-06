18661e193d9abe12ed7677743accfb54c1af9297
Sankalp <160457369+THEKNIGHTPROTOCOL@users.noreply.github.com>
2025-08-06 08:15:56 +0530

    Update Addition of Two Matrices.java

--- Addition of Two Matrices.java
+++ Addition of Two Matrices.java
@@ -3,7 +3,7 @@ public class MatrixAddition {
         int[][] A = {{1, 2}, {3, 4}};
         int[][] B = {{5, 6}, {7, 8}};
         int[][] C = new int[2][2];
-
+ 
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 C[i][j] = A[i][j] + B[i][j];
